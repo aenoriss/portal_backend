c086541a95ada6a8d80b99563ee4515045b88776
aenoris <aenoriss@gmail.com>
2023-06-13 23:58:01 -0300

    asdasd

--- receive_stream.py
+++ receive_stream.py
@@ -1,39 +1,29 @@
 import asyncio
 import websockets
-import binascii
-from io import BytesIO
-
 from PIL import Image, UnidentifiedImageError
-
-print(websockets)
 
 def is_valid_image(image_bytes):
     try:
-        Image.open(BytesIO(image_bytes))
-        # print("image OK")
+        Image.open(image_bytes)
         return True
-    except UnidentifiedImageError:
-        print("image invalid")
+    except (UnidentifiedImageError, OSError):
         return False
 
 async def handle_connection(websocket, path):
     while True:
         try:
             message = await websocket.recv()
-            print(message)
-            print(len(message))
-            if len(message) > 5000:
-                  if is_valid_image(message):
-                          #print(message)
-                          with open("image.jpg", "wb") as f:
-                                f.write(message)
-
-            print()
+            if is_valid_image(message):
+                with open("image.jpg", "wb") as f:
+                    f.write(message)
+                    print("Image saved")
+            else:
+                print("Invalid image received")
         except websockets.exceptions.ConnectionClosed:
             break
 
 async def main():
-    server = await websockets.serve(handle_connection, '0.0.0.0')
+    server = await websockets.serve(handle_connection, '0.0.0.0', 3001)  # Replace 3001 with your desired port number
     await server.wait_closed()
 
 asyncio.run(main())
